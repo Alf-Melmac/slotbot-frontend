48c82c5fd9d0d115d94af3ee0f4c0941a9714e44
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-02-27 19:45:25 +0100

    TS React component template

---
+++ .idea/fileTemplates/TS React component.tsx
@@ -0,0 +1,13 @@
+type ${NAME}Props = {
+    
+};
+
+export function ${NAME}(props: ${NAME}Props): JSX.Element {
+    const {} = props;
+
+    return (
+        <div>
+            $0
+        </div>
+    );
+}
