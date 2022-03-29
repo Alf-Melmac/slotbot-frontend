641ca52d74234c8e46ef4d4e0deeb41d5742ba2c
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-03-30 00:33:52 +0200

    SocialButtons component

---
+++ src/features/startpage/SocialButtons.tsx
@@ -0,0 +1,68 @@
+import {ActionIcon, createStyles, Group} from "@mantine/core";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faDiscord, faTwitch, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+
+type SocialButtonsProps = {};
+
+export function SocialButtons(props: SocialButtonsProps): JSX.Element {
+	const useStyles = createStyles((theme) => ({
+		social: {
+			listStyle: 'none',
+			fontSize: theme.fontSizes.sm,
+			fontWeight: 'bold',
+			color: theme.white,
+
+			[theme.fn.smallerThan('lg')]: {
+				left: 'unset'
+			},
+
+			'li': {
+				display: 'inline-block',
+			}
+		},
+
+		socialTitle: {
+			paddingRight: '40px',
+			marginRight: 20,
+			position: 'relative',
+
+			'&:after': {
+				display: 'block',
+				content: '""',
+				width: '24px',
+				height: '2px',
+				backgroundColor: 'rgba(255,255,255,0.1)',
+				position: 'absolute',
+				right: 0,
+				top: '50%',
+			},
+		},
+	}));
+	const {classes} = useStyles();
+
+	return (
+		<ul className={classes.social}>
+			<li className={classes.socialTitle}>Folge uns</li>
+			<li>
+				<Group>
+					<ActionIcon component={'a'} target={'_blank'} rel={'noopener noreferrer'}
+								href={"https://armamachtbock.de/discord"}>
+						<FontAwesomeIcon icon={faDiscord} size={"lg"}/>
+					</ActionIcon>
+					<ActionIcon component={'a'} target={'_blank'} rel={'noopener noreferrer'}
+								href={"https://twitter.com/ArmaMachtBock"}>
+						<FontAwesomeIcon icon={faTwitter} size={"lg"}/>
+					</ActionIcon>
+					<ActionIcon component={'a'} target={'_blank'} rel={'noopener noreferrer'}
+								href={"https://armamachtbock.de/youtube"}>
+						<FontAwesomeIcon icon={faYoutube} size={"lg"}/>
+					</ActionIcon>
+					<ActionIcon component={'a'} target={'_blank'} rel={'noopener noreferrer'}
+								href={"https://www.twitch.tv/ArmaMachtBock"}>
+						<FontAwesomeIcon icon={faTwitch} size={"lg"}/>
+					</ActionIcon>
+				</Group>
+			</li>
+		</ul>
+	);
+}
