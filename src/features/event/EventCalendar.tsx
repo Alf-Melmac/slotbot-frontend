6d4fcf5832b30fa3643dbe0a2b0217b7a5fc5d03
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-04-21 22:13:57 +0200

    Fix event tooltip position

--- src/features/event/EventCalendar.tsx
+++ src/features/event/EventCalendar.tsx
@@ -1,22 +1,37 @@
 import FullCalendar, {EventContentArg} from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import de from "@fullcalendar/core/locales/de";
-import {ColorSwatch, Group, Text, Tooltip} from "@mantine/core";
+import {ColorSwatch, createStyles, Text, Tooltip} from "@mantine/core";
+import {Bold} from '../../components/Text/Bold';
+
+const useStyles = createStyles((theme) => ({
+	eventType: {
+		minWidth: 10,
+		minHeight: 10,
+	},
+
+	eventTitleWrapper: {
+		overflow: 'hidden',
+	},
+}));
 
 type EventCalendarProps = {};
 
 export function EventCalendar(props: EventCalendarProps): JSX.Element {
 	const {} = props;
+	const {classes} = useStyles();
+
 	const eventContent = (arg: EventContentArg) => {
 		return (
-			<Group noWrap style={{minWidth: 0, gap: 2, overflow: "hidden"}}>
-				<ColorSwatch color={arg.backgroundColor} size={10} style={{minWidth: 10}}/>
-				<Tooltip label={<span dangerouslySetInnerHTML={{__html: arg.event.extendedProps.description}}></span>}>
-					<Text>{arg.timeText} {arg.event.title}</Text>
+			<>
+				<ColorSwatch color={arg.backgroundColor} size={10} className={classes.eventType} mx={2}/>
+				<Tooltip className={classes.eventTitleWrapper}
+						 label={<span dangerouslySetInnerHTML={{__html: arg.event.extendedProps.description}}></span>}>
+					<Text>{arg.timeText} <Bold>{arg.event.title}</Bold></Text>
 				</Tooltip>
-			</Group>
+			</>
 		);
-	}
+	};
 
 	return (
 		<FullCalendar
@@ -25,9 +40,9 @@ export function EventCalendar(props: EventCalendarProps): JSX.Element {
 			locale={de}
 			eventSources={[
 				{
-					url: 'http://localhost:8090/events/list',
-					color: 'blue'
-				}
+					url: '/events/list',
+					color: 'blue',
+				},
 			]}
 			eventContent={eventContent}
 		/>
