7cc37fdfef64e65b0aa26a5be4d9db9887857060
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-07-06 22:35:00 +0200

    Add new EventWizard page

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,9 @@ import {EventRoutes} from './features/event/routes/EventRoutes';
 export function Router(): JSX.Element {
 	return (
 		<Routes>
-			<Route path="/" element={<StartPage/>}/>
-			<Route path="events/*" element={<EventRoutes/>}/>
-			<Route path="/404" element={<Title>404 :(</Title>}/>
+			<Route path='/' element={<StartPage/>}/>
+			<Route path='events/*' element={<EventRoutes/>}/>
+			<Route path='/404' element={<Title>404 :(</Title>}/>
 		</Routes>
 	);
 }
--- src/features/event/routes/EventRoutes.tsx
+++ src/features/event/routes/EventRoutes.tsx
@@ -1,12 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
 import {EventDetails} from '../details/EventDetails';
 import {Events} from '../calendar/Events';
+import {EventWizard} from '../wizard/EventWizard';
 
 export function EventRoutes(): JSX.Element {
 	return (
 		<Routes>
 			<Route path="" element={<Events/>}/>
 			<Route path=":eventId" element={<EventDetails/>}/>
+			<Route path="/new" element={<EventWizard/>}/>
 		</Routes>
 	);
 }
