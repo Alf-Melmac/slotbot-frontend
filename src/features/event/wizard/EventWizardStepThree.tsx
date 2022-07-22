cd7c784f5e18de041e8cb042ec12f9968738bd0e
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-07-23 00:16:05 +0200

    SquadListEntrySettings header

--- src/features/event/wizard/EventWizardStepThree.tsx
+++ src/features/event/wizard/EventWizardStepThree.tsx
@@ -1,44 +1,22 @@
-import {
-	ActionIcon,
-	Box,
-	Checkbox,
-	createStyles,
-	Group,
-	Menu,
-	Modal,
-	NumberInput,
-	Text,
-	TextInput,
-	Title,
-} from '@mantine/core';
+import {Box, Checkbox, Group, NumberInput, TextInput, Title} from '@mantine/core';
 import {EventWizardStepProps} from './EventWizard';
 import {TEXT} from '../../../utils/maxLength';
 import {randomId} from '@mantine/hooks';
 import {AddButton} from '../../../components/Form/AddButton';
 import {SlotDto, SquadDto} from '../eventTypes';
 import {includes, sortBy} from 'lodash';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faEllipsisH, faTrashCan, faUserGear} from '@fortawesome/free-solid-svg-icons';
-import {useState} from 'react';
-
-const useStyles = createStyles((theme) => ({
-	rootGrow: {
-		flex: '1 1 0 !important',
-	},
-}));
+import {SquadListEntrySettings} from './SquadListEntrySettings';
 
 export function EventWizardStepThree(props: EventWizardStepProps): JSX.Element {
 	const {form} = props;
-	const {classes} = useStyles();
 
 	const squadList = form.values.squadList.map((squad, squadIndex) => (
 		<Box key={squad.id} mt={'sm'} mb={'xs'}>
 			<Group spacing={5}>
 				<TextInput placeholder={'Squad Name'} maxLength={TEXT} required
 						   styles={{root: {flexGrow: '1 !important'}}}
-					// className={{root: classes.rootGrow}}
 						   {...form.getInputProps(`squadList.${squadIndex}.name`)}/>
-				<SquadListEntrySettings/>
+				<SquadListEntrySettings entry={form.values.squadList[squadIndex]}/>
 			</Group>
 			<Box ml={'md'}>
 				{form.values.squadList[squadIndex].slotList?.map((slot, slotIndex) => (
@@ -47,7 +25,7 @@ export function EventWizardStepThree(props: EventWizardStepProps): JSX.Element {
 									 {...form.getInputProps(`squadList.${squadIndex}.slotList.${slotIndex}.number`)}/>
 						<TextInput styles={{root: {flexGrow: '1 !important'}}}
 								   {...form.getInputProps(`squadList.${squadIndex}.slotList.${slotIndex}.name`)}/>
-						<SquadListEntrySettings/>
+						<SquadListEntrySettings entry={form.values.squadList[squadIndex].slotList[slotIndex]} slot/>
 					</Group>
 				))}
 				<AddButton label={'Slot hinzufügen'} mt={'xs'}
@@ -73,30 +51,6 @@ export function EventWizardStepThree(props: EventWizardStepProps): JSX.Element {
 					  {...form.getInputProps('reserveParticipating', {type: 'checkbox'})}/>
 		</>
 	);
-}
-
-function SquadListEntrySettings(): JSX.Element {
-	const [opened, setOpened] = useState(false);
-
-	return <>
-		<Modal opened={opened} onClose={() => setOpened(false)} title={'Regeln für Squad'}>
-			<Text>Reservierung</Text>
-		</Modal>
-		<Menu>
-			<Menu.Target>
-				<ActionIcon size={'lg'}>
-					<FontAwesomeIcon icon={faEllipsisH} size={'lg'}/>
-				</ActionIcon>
-			</Menu.Target>
-			<Menu.Dropdown>
-				<Menu.Item icon={<FontAwesomeIcon icon={faUserGear}/>}
-						   onClick={() => setOpened(true)}>
-					Regeln
-				</Menu.Item>
-				<Menu.Item icon={<FontAwesomeIcon icon={faTrashCan}/>}>Löschen</Menu.Item>
-			</Menu.Dropdown>
-		</Menu>
-	</>;
 }
 
 function buildNewSlot(form: EventWizardStepProps['form']): SlotDto {
---
+++ src/features/event/wizard/SquadListEntrySettings.tsx
@@ -0,0 +1,50 @@
+import {SlotDto, SquadDto} from '../eventTypes';
+import {useState} from 'react';
+import {ActionIcon, Menu, Modal, Text} from '@mantine/core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEllipsisH, faTrashCan, faUserGear} from '@fortawesome/free-solid-svg-icons';
+
+type SquadListEntrySettingsProps = {
+	entry: SquadDto | SlotDto;
+	slot?: boolean;
+};
+
+export function SquadListEntrySettings(props: SquadListEntrySettingsProps): JSX.Element {
+	const {entry, slot = false} = props;
+	const [opened, setOpened] = useState(false);
+
+	return <>
+		<Modal opened={opened} onClose={() => setOpened(false)} title={getHeader(entry, slot)}>
+			<Text>Reservierung</Text>
+		</Modal>
+		<Menu>
+			<Menu.Target>
+				<ActionIcon size={'lg'}>
+					<FontAwesomeIcon icon={faEllipsisH} size={'lg'}/>
+				</ActionIcon>
+			</Menu.Target>
+			<Menu.Dropdown>
+				<Menu.Item icon={<FontAwesomeIcon icon={faUserGear}/>}
+						   onClick={() => setOpened(true)}>
+					Regeln
+				</Menu.Item>
+				<Menu.Item icon={<FontAwesomeIcon icon={faTrashCan}/>}>Löschen</Menu.Item>
+			</Menu.Dropdown>
+		</Menu>
+	</>;
+}
+
+function getHeader(entry: SquadDto | SlotDto, isSlot: boolean): string {
+	let header = 'Regeln für';
+	const slot: SlotDto | undefined = isSlot ? entry as SlotDto : undefined;
+	if (isSlot && slot?.number && !isNaN(slot.number)) {
+		header += ` (${slot.number})`;
+	}
+	if (entry.name) {
+		header += ` ${entry.name}`;
+	} else {
+		header += slot ? ' Slot' : ' Squad';
+	}
+
+	return header;
+}
