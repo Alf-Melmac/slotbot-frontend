8dc80ccfef20c4954892f21223e5d628f6a05fad
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-07-28 23:13:39 +0200

    Add chevron before first breadcrumb element

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,5 +1,7 @@
-import {Breadcrumbs, Text} from '@mantine/core';
+import {Breadcrumbs, Group, Text} from '@mantine/core';
 import {AnchorLink} from './Text/AnchorLink';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircleChevronLeft} from '@fortawesome/free-solid-svg-icons';
 
 type BreadcrumbItem = {
 	title: JSX.Element | string;
@@ -14,7 +16,16 @@ export function Breadcrumb(props: BreadcrumbProps): JSX.Element {
 	const breadcrumbItems = props.items.map((item, index) => (
 		//Double text to center vertically
 		item.href ?
-			<AnchorLink to={item.href} key={index}><Text>{item.title}</Text></AnchorLink>
+			<AnchorLink to={item.href} key={index}>
+				{index == 0 ?
+					<Group spacing={4}>
+						<FontAwesomeIcon icon={faCircleChevronLeft} style={{fontSize: 14, paddingTop: 2}}/>
+						<Text>{item.title}</Text>
+					</Group>
+					:
+					<Text>{item.title}</Text>
+				}
+			</AnchorLink>
 			:
 			<Text key={index}><Text>{item.title}</Text></Text>
 	));
