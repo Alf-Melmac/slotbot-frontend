e95bd3f5891a5388625707b2f6bae400d74edbdf
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2022-10-18 23:40:31 +0200

    Send event action text input changes to backend

--- src/features/event/action/EventActionTextInputEditMode.tsx
+++ src/features/event/action/EventActionTextInputEditMode.tsx
@@ -2,20 +2,38 @@ import {InlineEditableText} from '../../../components/Input/InlineEditable/Inlin
 import {useState} from 'react';
 import {FormTextInputProps} from './EventActionTextInput';
 import {useFormContext} from '../../../contexts/event/action/EventActionFormContext';
+import slotbotServerClient, {voidFunction} from '../../../hooks/slotbotServerClient';
+import {useEventPage} from '../../../contexts/event/EventPageContext';
+import {useMutation} from '@tanstack/react-query';
+import {AxiosError} from 'axios';
+import {showNotification} from '@mantine/notifications';
 
 export function EventActionTextInputEditMode(props: FormTextInputProps): JSX.Element {
 	const {inputProps, formPath} = props;
+	const eventId = useEventPage();
 	const form = useFormContext();
 	const formInputProps = form.getInputProps(formPath);
 
-	// const postTextChange = () => slotbotServerClient.put(`/events/${useEventPage()}/edit/text`).then((res) => res.data);
 	const [oldValue, setOldValue] = useState<string>(formInputProps.value || '');
+	const postTextChange = () => slotbotServerClient.put(`/events/${eventId}/edit/text`, {
+		key: formPath,
+		value: formInputProps.value
+	}).then(voidFunction);
+	const {mutate} = useMutation<void, AxiosError>(postTextChange, {
+		onSuccess: () => {
+			setOldValue(formInputProps.value);
+			showNotification({title: 'Gespeichert', message: formInputProps.value, color: 'green'});
+		},
+		onError: error => {
+			showNotification({
+				title: `Speichern fehlgeschlagen. ${error.code ? `(${error.code})` : ''}`,
+				message: error.message,
+				color: 'red',
+			});
+		},
+	});
 	return (
 		<InlineEditableText {...inputProps} position={'group'} {...formInputProps}
-							onSubmit={() => {
-								console.log(formInputProps.value); //TODO mutate
-								setOldValue(formInputProps.value);
-								/*@ts-ignore text input must accept strings*/
-							}} onCancel={() => form.setFieldValue(formPath, oldValue)}/>
+							onSubmit={mutate} onCancel={() => form.setFieldValue(formPath, oldValue)}/>
 	);
 }
