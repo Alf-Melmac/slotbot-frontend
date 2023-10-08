ceca85e5bff33b86d2aaeae99aafec2d4729f77b
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2023-10-08 21:56:03 +0200

    Apply browserlist with browserlist to esbuild Group fullcalendar update Dependency updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import browserslistToEsbuild from 'browserslist-to-esbuild';
 
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		target: browserslistToEsbuild([">0.2%", "not dead", "not op_mini all"]),
+	},
 	base: '/',
 	server: {
 		open: true,
