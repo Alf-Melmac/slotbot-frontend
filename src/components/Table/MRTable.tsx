db28500a55ddfb6df82eab4310e14878396984f4
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2024-02-20 22:10:03 +0000

    Components part 3

--- src/components/Table/MRTable.tsx
+++ src/components/Table/MRTable.tsx
@@ -1,6 +1,7 @@
 import {MantineReactTable, MRT_TableOptions} from 'mantine-react-table';
 import {PartialExcept} from '../../utils/typesHelper';
 import {JSX} from 'react';
+import classes from './MRTable.module.css';
 
 /**
  * Wrapper for {@link MantineReactTable} which tries to reproduce mantines {@link Table} default styling
@@ -10,20 +11,9 @@ export function MRTable<D extends Record<string, any>>(props: Readonly<PartialEx
 		<MantineReactTable
 			enablePagination={false} enableColumnActions={false} enableSorting={false} enableTopToolbar={false}
 			enableStickyHeader state={{density: 'xs'}} mantinePaperProps={{withBorder: false}}
-			mantineTableBodyCellProps={{
-				sx: {
-					paddingTop: '7px !important',
-					paddingBottom: '7px !important',
-					backgroundColor: 'unset',
-				},
-			}}
+			mantineTableBodyCellProps={{className: classes.bodyCell}}
 			//Force progress bar size
-			mantineBottomToolbarProps={{
-				sx: {
-					height: 8,
-					minHeight: 8,
-				},
-			}}
+			mantineBottomToolbarProps={{className: classes.bottomToolbar}}
 			enableBottomToolbar={props.state?.showProgressBars}
 			{...props}/>
 	);
