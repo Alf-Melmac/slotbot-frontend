be35d34dabb87f4071b909640788f834549dfcc4
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2024-03-09 00:06:31 +0100

    Dependency updates

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,14 @@ export function App(): JSX.Element {
 		},
 	});
 
-	const router = createBrowserRouter(routes, {future: {v7_normalizeFormMethod: true, v7_fetcherPersist: true}});
+	const router = createBrowserRouter(routes, {
+		future: {
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_relativeSplatPath: true,
+		},
+	});
 
 	return (
 		<Suspense fallback={<Skeleton/>}>
