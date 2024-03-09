08d2646fb82d776840a17a60decdd4bd4583a00e
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2024-03-09 18:59:12 +0100

    Remove unnecessary noinspect

--- src/components/Text/UnstyledAnchorLink.tsx
+++ src/components/Text/UnstyledAnchorLink.tsx
@@ -1,9 +1,8 @@
 import {AnchorLink, ReactRouterAnchorProps} from './AnchorLink';
-import {JSX} from 'react';
+import {JSX, PropsWithChildren} from 'react';
 import cx from 'clsx';
 import classes from './UnstyledAnchorLink.module.css';
 
-export function UnstyledAnchorLink(props: Readonly<ReactRouterAnchorProps>): JSX.Element {
-    // noinspection RequiredAttributes children is provided by props
-    return <AnchorLink {...props} className={cx(classes.link, props.className)}/>;
+export function UnstyledAnchorLink(props: Readonly<PropsWithChildren<ReactRouterAnchorProps>>): JSX.Element {
+	return <AnchorLink {...props} className={cx(classes.link, props.className)}>{props.children}</AnchorLink>;
 }
