1e0343f57a7a11ecac416b4f1372c9a0e10e8675
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2024-06-08 21:44:10 +0200

    Update profile info loading to include last event info

--- src/features/profile/loading/ProfileInfoLoading.tsx
+++ src/features/profile/loading/ProfileInfoLoading.tsx
@@ -1,17 +1,28 @@
-import {Center, Paper, Skeleton, Stack, Title} from '@mantine/core';
+import {Center, Group, Paper, Skeleton, Stack, Title, useMantineTheme} from '@mantine/core';
 import {T} from '../../../components/T';
 import {JSX} from 'react';
 
 export function ProfileInfoLoading(): JSX.Element {
+	const theme = useMantineTheme();
+
 	return (
 		<Stack>
 			<Center>
 				<Paper withBorder w={{base: '100%', md: '33%'}} p={'lg'}>
 					<Stack align={'center'} gap={'xs'}>
 						<Skeleton circle height={84}/>
-						<Skeleton width={'70%'} height={35}/>
-						<Skeleton width={'20%'} height={31} mt={'xl'}/>
-						<Title order={5}><T k={'profile.info.participatedEvents'}/></Title>
+						<Skeleton width={'70%'} height={theme.headings.sizes.h2.lineHeight}/>
+
+						<Group mt={'xl'} justify={'center'}>
+							<Stack align={'center'} gap={'xs'}>
+								<Title order={5}><T k={'profile.info.participatedEvents'}/></Title>
+								<Skeleton width={'40%'} height={theme.headings.sizes.h5.lineHeight}/>
+							</Stack>
+							<Stack align={'center'} gap={'xs'}>
+								<Title order={5}><T k={'profile.info.lastEvent'}/></Title>
+								<Skeleton height={theme.headings.sizes.h5.lineHeight}/>
+							</Stack>
+						</Group>
 					</Stack>
 				</Paper>
 			</Center>
