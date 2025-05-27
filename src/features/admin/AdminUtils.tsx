71dd71cfa082ab7e09e8dd7d0d681162ae981ee3
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2025-05-27 22:46:25 +0200

    Leave guild admin action

--- src/features/admin/AdminUtils.tsx
+++ src/features/admin/AdminUtils.tsx
@@ -1,23 +1,26 @@
-import {Button, Stack} from '@mantine/core';
+import {Button, Group, Stack, TextInput} from '@mantine/core';
 import slotbotServerClient, {voidFunction} from '../../hooks/slotbotServerClient';
 import {JSX, useEffect, useState} from 'react';
 import {useMutation} from '@tanstack/react-query';
 import {AxiosError} from 'axios';
 import {errorNotification, successNotification} from '../../utils/notificationHelper';
-import {T} from '../../components/T';
 import {useTranslatedDocumentTitle} from '../../hooks/useDocumentTitle';
 
 export default function AdminUtils(): JSX.Element {
 	useTranslatedDocumentTitle('documentTitle.admin.utils');
 	const [action, setAction] = useState('');
-	const postAction = () => slotbotServerClient.post(`/admin/utils/${action}`).then(voidFunction);
+	const [body, setBody] = useState('');
+	const postAction = () => slotbotServerClient.post(`/admin/utils/${action}`, body, {headers: {'Content-Type': 'text/plain'}}).then(voidFunction);
 	const {mutate} = useMutation<void, AxiosError>({
 		mutationFn: postAction,
 		onSuccess: () => {
 			successNotification(action);
-			setAction('');
 		},
 		onError: errorNotification,
+		onSettled: () => {
+			setAction('');
+			setBody('');
+		}
 	});
 	useEffect(() => {
 		if (!action) return;
@@ -27,17 +30,24 @@ export default function AdminUtils(): JSX.Element {
 	return (
 		<Stack>
 			<Button size={'lg'} onClick={() => setAction('listFiles')}>
-				<T k={'admin.utils.listFiles'}/>
+				Update file list
 			</Button>
 			<Button size={'lg'} color={'red'} onClick={() => setAction('deleteUnusedEventTypes')}>
-				<T k={'admin.utils.deleteUnusedEventTypes'}/>
+				Remove unused event types
 			</Button>
 			<Button size={'lg'} onClick={() => setAction('rebuildEventNotifications')}>
-				<T k={'admin.utils.rebuildEventNotifications'}/>
+				Rebuild event notifications
 			</Button>
 			<Button size={'lg'} onClick={() => setAction('rebuildCalendars')}>
-				<T k={'admin.utils.rebuildCalendars'}/>
+				Rebuild global ICS calendars
 			</Button>
+			<Group align={'flex-end'} grow>
+				<TextInput label={'Guild ID'} placeholder={'123456789012345678'} value={body}
+						   onChange={(e) => setBody(e.currentTarget.value)}/>
+				<Button size={'lg'} color={'red'} onClick={() => setAction('leaveGuild')}>
+					Leave Guild
+				</Button>
+			</Group>
 		</Stack>
 	);
 }
