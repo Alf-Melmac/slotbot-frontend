3f1a7eb72fdfe122eb37e898edd2359d112d5169
Alf <62615304+alf-melmac@users.noreply.github.com>
2025-12-06 23:42:08 +0100

    Fix notification settings displaying old state after update

--- src/features/profile/GlobalNotificationSettings.tsx
+++ src/features/profile/GlobalNotificationSettings.tsx
@@ -3,10 +3,10 @@ import {ActionIcon, Group, NumberInput, Text, Title} from '@mantine/core';
 import {UserOwnProfileDto} from './profileTypes';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTrashCan} from '@fortawesome/free-solid-svg-icons';
-import {useForm} from '@mantine/form';
+import {useForm, UseFormReturnType} from '@mantine/form';
 import {AddButton} from '../../components/Button/AddButton';
 import slotbotServerClient from '../../hooks/slotbotServerClient';
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {AxiosError} from 'axios';
 import {ButtonWithDisabledTooltip} from '../../components/Button/ButtonWithDisabledTooltip';
 import {errorNotification, successNotification} from '../../utils/notificationHelper';
@@ -19,11 +19,13 @@ type GlobalNotificationSettingsProps = Pick<UserOwnProfileDto, 'notificationSett
 
 export function GlobalNotificationSettings(props: Readonly<GlobalNotificationSettingsProps>): JSX.Element {
 	const form = useForm<GlobalNotificationSettingsProps>({
+		mode: 'uncontrolled',
 		initialValues: {
 			notificationSettings: props.notificationSettings,
 		},
 	});
 
+	const queryClient = useQueryClient();
 	const postNotificationSettings = () => slotbotServerClient.put('/notifications/own', form.values.notificationSettings).then((res) => res.data);
 	const {
 		mutate,
@@ -37,6 +39,12 @@ export function GlobalNotificationSettings(props: Readonly<GlobalNotificationSet
 				: <T k={'notifications.enabled'} count={noOfNotifications} countAsArgs/>);
 			form.setValues({notificationSettings: data});
 			form.resetDirty({notificationSettings: data});
+			queryClient.setQueryData(['ownProfile'], (oldData: UserOwnProfileDto) => {
+				return {
+					...oldData,
+					notificationSettings: data,
+				} satisfies UserOwnProfileDto;
+			});
 		},
 		onError: errorNotification,
 	});
@@ -50,17 +58,20 @@ export function GlobalNotificationSettings(props: Readonly<GlobalNotificationSet
 				                 multiline tooltipWidth={300} tooltipPosition={'right'}/>
 			</Title>
 
-			{form.values.notificationSettings.map((setting, index) => {
+			{form.getValues().notificationSettings.map((setting, index) => {
 					const hoursInputProps = form.getInputProps(`notificationSettings.${index}.hoursBeforeEvent`);
 					const minutesInputProps = form.getInputProps(`notificationSettings.${index}.minutesBeforeEvent`);
-					return <Group key={setting.id}>
-						<ActionIcon color={'gray'} variant={'subtle'} onClick={() => form.removeListItem('notificationSettings', index)}>
+					return <Group key={setting.id ?? `i${index}`}>
+						<ActionIcon color={'gray'} variant={'subtle'}
+						            onClick={() => form.removeListItem('notificationSettings', index)}>
 							<FontAwesomeIcon icon={faTrashCan}/>
 						</ActionIcon>
-						<NumberInput {...hoursInputProps} min={0}
-						             suffix={` ${t('hour', getOptions(hoursInputProps.value))}`}/>
-						<NumberInput {...minutesInputProps} min={0}
-						             suffix={` ${t('minute', getOptions(minutesInputProps.value))}`}/>
+						<NumberInput key={`notificationSettings.${index}.hoursBeforeEvent`} {...hoursInputProps}
+						             min={0}
+						             suffix={` ${t('hour', getOptions(form, index, 'hoursBeforeEvent'))}`}/>
+						<NumberInput key={`notificationSettings.${index}.minutesBeforeEvent`} {...minutesInputProps}
+						             min={0}
+						             suffix={` ${t('minute', getOptions(form, index, 'minutesBeforeEvent'))}`}/>
 						<Text><T k={'notifications.input.suffix'}/></Text>
 					</Group>;
 				},
@@ -72,14 +83,14 @@ export function GlobalNotificationSettings(props: Readonly<GlobalNotificationSet
 					           minutesBeforeEvent: 0,
 				           })}/>
 				{(form.values.notificationSettings.length > 0 || form.isDirty()) &&
-                    <ElementWithInfo
-                        text={<ButtonWithDisabledTooltip color={'green'} onClick={() => mutate()}
+					<ElementWithInfo
+						text={<ButtonWithDisabledTooltip color={'green'} onClick={() => mutate()}
 						                                 disabled={!form.isDirty()} tooltip={'noChanges'}
 						                                 loading={isPending}>
 							<T k={'notifications.save'}/></ButtonWithDisabledTooltip>
 						}
-                        tooltip={<T k={'profile.notifications.save.tooltip'}/>}
-                        iconPosition={YPosition.LEFT} multiline tooltipWidth={250} tooltipPosition={'left'}/>
+						tooltip={<T k={'profile.notifications.save.tooltip'}/>}
+						iconPosition={YPosition.LEFT} multiline tooltipWidth={250} tooltipPosition={'left'}/>
 				}
 			</Group>
 		</>
@@ -89,6 +100,7 @@ export function GlobalNotificationSettings(props: Readonly<GlobalNotificationSet
 /**
  * getRelativeTimeInputFormatterTranslationOptions
  */
-function getOptions(value: string | undefined): TranslationOptions {
-	return {count: Number.parseInt(value ?? '0')};
+function getOptions(form: UseFormReturnType<GlobalNotificationSettingsProps>, index: number, key: 'hoursBeforeEvent' | 'minutesBeforeEvent'): TranslationOptions {
+	const notificationSettingDtoElement = form.getValues().notificationSettings[index][key];
+	return {count: notificationSettingDtoElement};
 }
